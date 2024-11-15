565f471e95a39ec4bf27f0722a650c39d8024bbc
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-15 09:40:41 -0600

    feat: `lines` block
    This allows for a nice api for working with lines

--- src/utilities/lines.test.ts
+++ src/utilities/lines.test.ts
@@ -1,4 +1,5 @@
-import { describe, it, expect } from 'vitest';
+import os from 'node:os';
+import { describe, expect, it } from 'vitest';
 import * as lines from './lines';
 
 describe('get', () => {
@@ -12,5 +13,37 @@ describe('get', () => {
 		const content = 'hello\\nhello\r\nhello';
 
 		expect(lines.get(content)).toStrictEqual(['hello\\nhello', 'hello']);
+	});
+});
+
+describe('join', () => {
+	it('Joins correctly', () => {
+		const ls = ['hello\\nhello', 'hello'];
+
+		expect(lines.join(ls)).toStrictEqual(`hello\\nhello${os.EOL}hello`);
+	});
+
+	it('Joins correctly with `lineNumbers`', () => {
+		const ls = ['hello\\nhello', 'hello'];
+
+		expect(lines.join(ls, { lineNumbers: true })).toStrictEqual(
+			` 1 hello\\nhello${os.EOL} 2 hello`
+		);
+	});
+
+	it('Joins correctly with `prefix`', () => {
+		const ls = ['hello\\nhello', 'hello'];
+
+		expect(lines.join(ls, { prefix: () => ' + ' })).toStrictEqual(
+			` + hello\\nhello${os.EOL} + hello`
+		);
+	});
+
+	it('Adds `prefix` before line numbers', () => {
+		const ls = ['hello\\nhello', 'hello'];
+
+		expect(lines.join(ls, { prefix: () => ' + ', lineNumbers: true })).toStrictEqual(
+			` +  1 hello\\nhello${os.EOL} +  2 hello`
+		);
 	});
 });
--- src/utilities/lines.ts
+++ src/utilities/lines.ts
@@ -1,16 +1,28 @@
 import os from 'node:os';
 import { leftPadMin } from './pad';
 
+/** Regex used to split on new lines
+ *
+ * ```
+ * /\n|\r\n/g
+ * ```
+ */
 export const NEW_LINE_REGEX = /\n|\r\n/g;
 
 /** Splits str into an array of lines.
  *
  * @param str
  * @returns
+ *
+ * ## Usage
+ *
+ * ```ts
+ * lines.split("hello\\nhello\nhello"); // ["hello\\nhello", "hello"]
+ * ```
  */
-const get = (str: string): string[] => str.split(/\n|\r\n/g);
+const get = (str: string): string[] => str.split(NEW_LINE_REGEX);
 
-type Options = {
+export type Options = {
 	lineNumbers: boolean;
 	prefix: (line: number, lineCount: number) => string;
 };
@@ -19,6 +31,22 @@ type Options = {
  *
  * @param lines
  * @returns
+ *
+ * ## Usage
+ *
+ * ```ts
+ * lines.join(["1", "2", "3"]); // "1\n2\n3" or on windows "1\r\n2\r\n3"
+ *
+ * // add line numbers
+ * lines.join(["import { } from '.'", "console.log('test')"], { lineNumbers: true });
+ * // 1 import {  } from '.'
+ * // 2 console.log('test')
+ *
+ * // add a custom prefix
+ * lines.join(["import { } from '.'", "console.log('test')"], { prefix: () => " + " });
+ * // + import {  } from '.'
+ * // + console.log('test')
+ * ```
  */
 const join = (lines: string[], { lineNumbers = false, prefix }: Partial<Options> = {}): string => {
 	let transformed = lines;
@@ -27,6 +55,10 @@ const join = (lines: string[], { lineNumbers = false, prefix }: Partial<Options>
 		const length = lines.length.toString().length + 1;
 
 		transformed = transformed.map((line, i) => `${leftPadMin(`${i + 1}`, length)} ${line}`);
+	}
+
+	if (prefix !== undefined) {
+		transformed = transformed.map((line, i) => `${prefix(i, lines.length)}${line}`);
 	}
 
 	return transformed.join(os.EOL);
