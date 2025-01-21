4c88419a82d8b3ae702e352fbb7293291d17085d
Aidan Bleser <aidanbleser35@gmail.com>
2025-01-21 15:25:01 -0600

    feat: `utils/time` block
    Includes some constants as well as a `formatDuration` function useful for beautifying ms durations

---
+++ src/utils/time.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { DAY, formatDuration, HOUR, MINUTE, SECOND } from './time';
+
+describe('formatDuration', () => {
+	it('correctly formats durations', () => {
+		expect(formatDuration(500)).toBe('500ms');
+		expect(formatDuration(SECOND)).toBe('1s');
+		expect(formatDuration(MINUTE)).toBe('1min');
+		expect(formatDuration(HOUR)).toBe('1h');
+		expect(formatDuration(DAY)).toBe('24h');
+	});
+
+	it('correctly handles transform function', () => {
+		expect(formatDuration(SECOND / 55, (num) => num.toFixed(2))).toBe('18.18ms');
+		expect(formatDuration(MINUTE / 55, (num) => num.toFixed(2))).toBe('1.09s');
+		expect(formatDuration(HOUR / 55, (num) => num.toFixed(2))).toBe('1.09min');
+		expect(formatDuration(DAY / 55, (num) => num.toFixed(2))).toBe('26.18min');
+	});
+});
